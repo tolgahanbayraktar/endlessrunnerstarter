0f4365c7d3d7ce1a2ba270efd386203679caafb5
tolgahanbayraktar <tolgahanbayraktar@gmail.com>
2017-08-19 12:46:00 +0300

    Endless Runner Starter Demo
    Endless Runner Starter Demo

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	public DeathMenu deathMenu;
+
+	private float score = 0.0f;
+	private int difficultyLevel = 1;
+	private int maxDifficultyLevel = 10;
+	private int scoreToNextLevel = 10;
+	private bool isDead = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+		if (isDead)
+			return;
+		
+		if (score >= scoreToNextLevel)
+			LevelUp ();
+
+		score += Time.deltaTime * difficultyLevel;
+		scoreText.text = ((int)score).ToString ();
+	}
+
+	void LevelUp()
+	{
+		if (difficultyLevel == maxDifficultyLevel)
+			return;
+		
+
+		scoreToNextLevel *= 2;
+		difficultyLevel++;
+		GetComponent<PlayerMotor>().SetSpeed (difficultyLevel);
+	}
+
+	public void OnDeath()
+	{
+		isDead = true;
+
+		if(PlayerPrefs.GetFloat("Highscore") < score){
+			PlayerPrefs.SetFloat ("Highscore", score);
+		}
+		deathMenu.ToggleEndMenu (score);
+	}
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+	public GameObject[] tilePrefabs;
+
+	private Transform playerTransform;
+	private float spawnZ = -10.0f;
+	private float tileLength = 10.0f;
+	private float saveZone = 15.0f;
+	private int amnTilesOnScreen = 5;
+	private int lastPrefabIndex = 0;
+
+	private List<GameObject> activeTiles;
+
+	void Start()
+	{
+		activeTiles = new List<GameObject> ();
+
+		playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+		for (int i = 0; i < amnTilesOnScreen; i++) {
+
+			if (i < 2)
+				SpawnTile (0);
+			else
+				SpawnTile ();
+		}
+	}
+
+	void Update()
+	{
+		if (playerTransform.position.z - saveZone > (spawnZ-tileLength*amnTilesOnScreen)) {
+			SpawnTile ();
+			DeleteTile ();
+		}
+
+	}
+
+	void SpawnTile(int prefabIndex=-1)
+	{
+		GameObject go;
+
+		if (prefabIndex == -1)
+			go = Instantiate (tilePrefabs [RandomPrefabIndex ()]) as GameObject;
+		else
+			go = Instantiate (tilePrefabs [prefabIndex]) as GameObject;
+
+		print (prefabIndex);
+		go.transform.SetParent (transform);
+		go.transform.position = Vector3.forward * spawnZ;
+		spawnZ += tileLength;
+		activeTiles.Add (go);
+
+
+	}
+
+	void DeleteTile()
+	{
+		Destroy (activeTiles [0]);
+		activeTiles.RemoveAt (0);
+	}
+
+	int RandomPrefabIndex()
+	{
+		if (tilePrefabs.Length < 1)
+			return 0;
+
+		int randomIndex = lastPrefabIndex;
+		while (randomIndex == lastPrefabIndex) {
+			randomIndex = Random.Range (0, tilePrefabs.Length);
+		}
+		lastPrefabIndex = randomIndex;
+		return randomIndex;
+	}
+}
